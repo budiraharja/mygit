d38a6fdfe23f10cc09332d68088352f4738404fc
budiraharja <ib.budi.raharja@gmail.com>
2013-10-21 13:52:35 +0700

    test changes

--- MyGit/src/com/example/mygit/MainActivity.java
+++ MyGit/src/com/example/mygit/MainActivity.java
@@ -7,8 +7,8 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 	}
 
 }
